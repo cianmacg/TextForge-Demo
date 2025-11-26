74c8dfa47548ff9498f787fddadabd47fe267458
Cian Smith <scian94@gmail.com>
2025-11-26 11:19:28 +0000

    Initial Commit with Spring files.

---
+++ src/main/java/ie/atu/cloudnative/TextForge_Demo/TextForgeDemoApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cloudnative.TextForge_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextForgeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextForgeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/cloudnative/TextForge_Demo/TextForgeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cloudnative.TextForge_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextForgeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
