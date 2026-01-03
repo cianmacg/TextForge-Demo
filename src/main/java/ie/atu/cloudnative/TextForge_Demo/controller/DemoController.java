ae62fff11519525fd959bfe45a6663b4590c150a
cianmacg <scian94@gmail.com>
2026-01-03 16:25:57 +0000

    Added thymeleaf dependency. Added initial Controller, Request and template file.

---
+++ src/main/java/ie/atu/cloudnative/TextForge_Demo/controller/DemoController.java
@@ -0,0 +1,26 @@
+package ie.atu.cloudnative.TextForge_Demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import ie.atu.cloudnative.TextForge_Demo.request.TextRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class DemoController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("request", new TextRequest());
+        return "index";
+    }
+
+    @PostMapping("/process")
+    public String process(@ModelAttribute TextRequest request, Model model) {
+        String mockResult = "The external API would have " + request.getAction() + "ed: " + request.getContent();
+        model.addAttribute("result", mockResult);
+        return "index";
+    }
+}
---
+++ src/main/java/ie/atu/cloudnative/TextForge_Demo/request/TextRequest.java
@@ -0,0 +1,22 @@
+package ie.atu.cloudnative.TextForge_Demo.request;
+
+public class TextRequest {
+    private String content;
+    private String action;
+
+    public String getAction() {
+        return this.action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
